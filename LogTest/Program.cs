2a22af76aab1c21753d4b523b9ad7afaf4afb21d
CoronelExequiel <execoronel14f@gmail.com>
2022-09-20 18:47:59 -0300

    punto1

---
+++ LogTest/Program.cs
@@ -0,0 +1,10 @@
+﻿using NLog;
+class Program{
+    
+    static void Main(string[] args)
+    {
+        Logger logger = LogManager.GetCurrentClassLogger();  
+        logger.Info("Mensaje de informacion");
+        logger.Debug("Mensaje de debug");
+    }
+}
